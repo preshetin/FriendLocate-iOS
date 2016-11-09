301e1392f60a635132d8d3a5474311ae68fde7d3
Petr Reshetin <preshetin@gmail.com>
2016-11-09 15:45:33 +0300

    Some fixes

--- FriendLocate/ViewController.swift
+++ FriendLocate/ViewController.swift
@@ -15,11 +15,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var map: GMSMapView!
     
     let locationManager = CLLocationManager()
-    var currentUserUid: String {
-        get {
-            return (FIRAuth.auth()?.currentUser!.uid)!
-        }
-    }
+
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
@@ -35,33 +31,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 ]
                 ref.child("locations").child(user!.uid).setValue(locationToSave)
             })
-            
-            
-            let ref1 = FIRDatabase.database().reference(withPath: "locations")
-            ref1.observe(FIRDataEventType.value, with: { (snapshot) in
-                let savedLocations = snapshot.value as! [String : AnyObject]
-                for (_, loc) in savedLocations {
-                    if loc is [String:AnyObject] {
-                        let uid = loc["uid"]! as! String
-                        let latitude = loc["latitude"] as! CLLocationDegrees
-                        let longitude = loc["longitude"] as! CLLocationDegrees
-                        if self.currentUserUid != uid {
-                            print("Adding map marker for user \(uid)")
-                            let position = CLLocationCoordinate2DMake(latitude, longitude)
-                            let marker = GMSMarker(position: position)
-                            marker.title = loc["name"]! as? String
-                            marker.map = self.map
-                            
-                            let southWest = CLLocationCoordinate2DMake(latitude,longitude)
-                            let northEast = location.coordinate
-                            let bounds = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
-                            let camera = self.map.camera(for: bounds, insets:UIEdgeInsets.init(top: 50, left: 50, bottom: 50, right: 50))
-                            self.map.camera = camera!
-                            
-                        }
-                    }
-                }
-            })
         }
         self.locationManager.stopUpdatingLocation()
     }
@@ -75,8 +44,44 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         map.isMyLocationEnabled = true
         map.settings.myLocationButton = true
         
-        print("current user is \(self.currentUserUid)")
         
+        
+        FIRAuth.auth()?.signInAnonymously() { (user, error) in
+            if error != nil {
+                print("Sign in failed: \(error)")
+            } else {
+                print(user?.uid)
+            }
+        }
+        
+        let ref1 = FIRDatabase.database().reference(withPath: "locations")
+
+        ref1.observe(FIRDataEventType.value, with: { (snapshot) in
+            let savedLocations = snapshot.value as! [String : AnyObject]
+            for (_, loc) in savedLocations {
+                if loc is [String:AnyObject] {
+                    let uid = loc["uid"]! as! String
+                    let latitude = loc["latitude"] as! CLLocationDegrees
+                    let longitude = loc["longitude"] as! CLLocationDegrees
+                    if FIRAuth.auth()?.currentUser!.uid != uid {
+                        print("Adding map marker for user \(uid)")
+                        let position = CLLocationCoordinate2DMake(latitude, longitude)
+                        let marker = GMSMarker(position: position)
+                        marker.title = loc["name"]! as? String
+                        marker.map = self.map
+                        
+                        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse {
+                            let southWest = CLLocationCoordinate2DMake(latitude,longitude)
+                            let northEast = self.map.myLocation!.coordinate
+                            let bounds = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+                            let camera = self.map.camera(for: bounds, insets:UIEdgeInsets.init(top: 50, left: 50, bottom: 50, right: 50))
+                            self.map.camera = camera!                            
+                        }
+                    }
+                }
+            }
+            ref1.removeAllObservers()
+        })
     }
     
     override func didReceiveMemoryWarning() {
